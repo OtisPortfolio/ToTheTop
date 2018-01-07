16b5acb2668fe0583036dc12e514650493652eef
John R Otis Jr <otista_2000@hotmail.com>
2018-01-06 18:11:56 -0800

    Modularizing Abilities

---
+++ Source/ToTheTop/AbilityComponent.h
@@ -0,0 +1,29 @@
+// This project was created by John R. Otis Jr. and may not be used by anyone without explicit written consent by the author. Dec 2017
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AbilityComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOTHETOP_API UAbilityComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAbilityComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
---
+++ Source/ToTheTop/FireAOEComponent.h
@@ -0,0 +1,20 @@
+// This project was created by John R. Otis Jr. and may not be used by anyone without explicit written consent by the author. Dec 2017
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityComponent.h"
+#include "FireAOEComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOTHETOP_API UFireAOEComponent : public UAbilityComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
